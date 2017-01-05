84eb0b9eb4bb492cbbce2b97da34940e64a22f1e
Richard Jones <richard.jones@inviqa.com>
2017-01-05 10:05:04 +0000

    Add Thunder installation script

--- tools/docker/usr/local/share/drupal8/install_finalise_custom.sh
+++ tools/docker/usr/local/share/drupal8/install_finalise_custom.sh
@@ -1,1 +1,24 @@
 #!/bin/bash
+
+cd /app
+
+# Install any new dependencies
+composer install --no-interaction --optimize-autoloader
+
+cd /app/docroot
+
+# Wait for a database.
+while ! mysqladmin ping -h ${DRUPAL_DATABASE_HOST} -P ${DRUPAL_DATABASE_PORT} -u ${DRUPAL_DATABASE_USERNAME} -p${DRUPAL_DATABASE_PASSWORD} --silent; do
+    echo "Couldn't establish database connection. Trying again in 5 seconds..."
+    sleep 5
+done;
+
+# Check if Drupal is installed and act accordingly.
+if ! drush status bootstrap | grep -q Successful ; then
+    # Drupal is not installed.
+    drush si thunder -y
+    drush cache-rebuild
+fi
+
+# Drupal should have been installed by now.
+
