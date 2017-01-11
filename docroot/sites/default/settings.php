a083a2a6c140a9a78c1c962fa8e9afd742905b14
Andy Thompson <athompson@inviqa.com>
2017-01-11 11:28:53 +0000

    Add an environment variable based settings file
    Environment variable defaults added to docker-compose.yml
    gitignore updated as well to track changes to committed settings files

--- docroot/sites/default/settings.php
+++ docroot/sites/default/settings.php
@@ -1,6 +1,6 @@
 <?php
 
-foreach(array(__DIR__ . '/settings.global.php', __DIR__ . '/settings.docker.php', __DIR__ . '/settings.local.php') as $file) {
+foreach(array(__DIR__ . '/settings.global.php', __DIR__ . '/settings.env.php', __DIR__ . '/settings.local.php') as $file) {
     if (file_exists($file)) {
         include_once $file;
     }
