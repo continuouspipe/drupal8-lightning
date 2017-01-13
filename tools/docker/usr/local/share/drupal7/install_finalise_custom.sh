6509be71c20fbdf3821d125b7a840b3a04a17f49
Richard Jones <richard.jones@inviqa.com>
2017-01-13 14:51:21 +0000

    Add initial install script for D7

--- tools/docker/usr/local/share/drupal7/install_finalise_custom.sh
+++ tools/docker/usr/local/share/drupal7/install_finalise_custom.sh
@@ -1,1 +1,24 @@
 #!/bin/bash
+
+cd /app
+
+# Install any new dependencies
+composer install --no-interaction --optimize-autoloader
+
+cd /app/docroot
+
+# Wait for a database.
+while ! mysqladmin ping -h ${DRUPAL_DATABASE_HOST} -P ${DRUPAL_DATABASE_PORT} -u ${DRUPAL_DATABASE_USERNAME} -p${DRUPAL_DATABASE_PASSWORD} --silent; do
+    echo "Couldn't establish database connection. Trying again in 5 seconds..."
+    sleep 5
+done;
+
+# Check if Drupal is installed and act accordingly.
+if ! drush status bootstrap | grep -q Successful ; then
+    # Drupal is not installed.
+    drush si standard -y
+    mkdir -p sites/default/files
+    chown -R www-data sites/default/files
+    chgrp -R www-data sites/default/files
+fi
+
