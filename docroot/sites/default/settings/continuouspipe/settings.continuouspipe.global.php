94e09d5017fd17f7b4f8f584f74d4f359faf1575
Matt Chapman <chapabu101@gmail.com>
2017-02-13 09:02:26 +0000

    Removed prefix setting

--- docroot/sites/default/settings/continuouspipe/settings.continuouspipe.global.php
+++ docroot/sites/default/settings/continuouspipe/settings.continuouspipe.global.php
@@ -7,12 +7,12 @@
  */
 
 $databases['default']['default'] = array (
-    'database' => getenv('DRUPAL_DATABASE_NAME'),
-    'username' => getenv('DRUPAL_DATABASE_USERNAME'),
-    'password' => getenv('DRUPAL_DATABASE_PASSWORD'),
-    'prefix' => getenv('DRUPAL_DATABASE_PREFIX'),
-    'host' => getenv('DRUPAL_DATABASE_HOST'),
-    'port' => getenv('DRUPAL_DATABASE_PORT'),
+    'database' => $_ENV['DRUPAL_DATABASE_NAME'],
+    'username' => $_ENV['DRUPAL_DATABASE_USERNAME'],
+    'password' => $_ENV['DRUPAL_DATABASE_PASSWORD'],
+    'prefix' => '',
+    'host' => $_ENV['DRUPAL_DATABASE_HOST'],
+    'port' => $_ENV['DRUPAL_DATABASE_PORT'],
     'namespace' => 'Drupal\\Core\\Database\\Driver\\mysql',
     'driver' => 'mysql',
 );
